f6624046bfa6d5f8bca2739b39631867c5186ff5
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-08-26 20:12:17 -0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BienvenidoComponent} from './artifact/bienvenido/bienvenido.component';
+import {LoginComponent} from './artifact/login/login.component';
+import {ErrorComponent} from './artifact/error/error.component';
+import {UserComponent} from './entity/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bienvenido', component: BienvenidoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'error', component: ErrorComponent},
+  {path: 'user', component: UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
